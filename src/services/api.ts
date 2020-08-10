334206ede237918a8cb7d01465e524106e8e8617
Vinicius Miranda <vinniimiranda@gmail.com>
2020-08-10 09:14:50 -0300

    update api baseURL

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import Axios from 'axios'
 
 export const API = Axios.create({
-  baseURL: process.env.NODE_ENV !== 'production' ? 'https://seu-job-api.herokuapp.com' : 'http://192.168.0.104:3333',
+  baseURL: 'https://seu-job-api.herokuapp.com',
   headers: {
     'Content-Type': 'application/json'
   }
